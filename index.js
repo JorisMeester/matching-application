8a50907be2aeef97b7db559a28fc83d7bfbcc669
JorisMeester <jorismeester98@gmail.com>
2021-02-15 10:16:33 +0100

    hij doet t weer!

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/static/public'));
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname), 'views')
+// app.set('views', path.join(__dirname), 'views')
 app.get("/", (req, res) =>{
-    res.render('filter', {data : {interests : ['Men', 'Women', 'Everyone'], religions : ['Atheist', 'Buddhist','Christian', 'Hinduist', 'Islamic', 'Jewish']}});
+    res.render('filter', {data : {title : 'Filters', interests : ['Men', 'Women', 'Everyone'], religions : ['Atheist', 'Buddhist','Christian', 'Hinduist', 'Islamic', 'Jewish']}});
 });
 app.listen(9001);
