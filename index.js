adab743073eb2075ccfcd6d9f56c77c2439dabbd
JorisMeester <jorismeester98@gmail.com>
2021-03-02 10:58:34 +0100

    connection with db!

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const slug = require('slug');
+const mongoose = require('mongoose');
 //const path = require('path');
 const app = express();
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
+require('dotenv/config');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/static/public'));
 app.set('view engine', 'ejs')
 // app.set('views', path.join(__dirname), 'views')
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.render('filter', { data: { title: 'Filters', interests: ['Men', 'Women', 'Everyone'], religions: ['Atheist', 'Buddhist', 'Christian', 'Hinduist', 'Islamic', 'Jewish'] } });
 });
 
+mongoose.connect(
+  process.env.dbConnectionString,
+  { useUnifiedTopology: true, useNewUrlParser: true },
+  (req, res) => {
+    console.log("JOEPIE");
+  }
+);
+
+app.post('/', add)
+
+function add(req, res) {
+  console.log(req.body)
+  let searchQuery = slug(toString(req.body)).toLowerCase();
+  // data.push({
+  //   interest: req.body.interest,
+  //   minAge: req.body.minAge,
+  //   maxAge: req.body.maxAge,
+  //   religion: req.body.religion,
+  //   livingDistance: req.body.livingDistance,
+  //   place: req.body.place,
+  //   hobbies: req.body.hobbies
+  // })
+
+  res.redirect('/search?q=' + searchQuery)
+}
+
+//always at the bottom
 app.use((req, res, next) => {
   res.status(404).send("Sorry, this page doesn't exist.")
 })
-
-app.post('/', urlencodedParser, (req, res)=>{
-  console.log(req.body);
-})
-
-// function add(req, res) {
-//   console.log(req.body);
-//   var id = slug(req.body.id).toLowerCase();
-
-//   data.push({
-//     id: id,
-//     interest: req.body.interest,
-//     minAge: req.body.min-age,
-//     maxAge: req.body.max-age,
-//     religion: req.body.religion,
-//     livingDistance: req.body.living - distance,
-//     place: req.body.place,
-//     hobbies: req.body.hobbies
-//   })
-
-//   res.redirect('/' + id)
-// }
 
 app.listen(9001);
